a539abbaf1d491110af7d49fcb183bf90674d0b9
tomimandalap <tomi.mandala@privyid.tech>
2022-07-20 21:26:33 +0700

    feat: wording content admin and about

--- src/views/Admin/About.vue
+++ src/views/Admin/About.vue
@@ -1,5 +1,70 @@
+<script setup>
+import { useRoute } from "vue-router";
+const route = useRoute();
+</script>
+
 <template>
   <main>
-    <h1>DASBOARD ABOUT</h1>
+    <h1 class="title">DASBOARD {{ route.name }}</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+      quibusdam, dolor, alias nostrum quos odit minus laudantium nisi, omnis
+      doloremque eligendi accusamus? Distinctio consectetur itaque sunt
+      dignissimos, minus illum at veniam sit maiores explicabo eligendi qui eum
+      repellendus magni magnam consequatur! Saepe eligendi doloremque dolorum
+      facilis odit expedita aspernatur fugit sapiente iste quidem blanditiis
+      ducimus, minima voluptatibus. Ea ut quibusdam voluptate temporibus
+      corrupti adipisci eligendi impedit, sint odit perspiciatis assumenda quod
+      velit ullam repellendus ex facere sunt quaerat architecto? Illum itaque
+      aperiam esse odit qui non, quia tenetur maiores maxime. Accusamus error
+      sit dolor perspiciatis nam, voluptas autem omnis illum!
+    </p>
+    <br />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+      quibusdam, dolor, alias nostrum quos odit minus laudantium nisi, omnis
+      doloremque eligendi accusamus? Distinctio consectetur itaque sunt
+      dignissimos, minus illum at veniam sit maiores explicabo eligendi qui eum
+      repellendus magni magnam consequatur! Saepe eligendi doloremque dolorum
+      facilis odit expedita aspernatur fugit sapiente iste quidem blanditiis
+      ducimus, minima voluptatibus. Ea ut quibusdam voluptate temporibus
+      corrupti adipisci eligendi impedit, sint odit perspiciatis assumenda quod
+      velit ullam repellendus ex facere sunt quaerat architecto? Illum itaque
+      aperiam esse odit qui non, quia tenetur maiores maxime. Accusamus error
+      sit dolor perspiciatis nam, voluptas autem omnis illum!
+    </p>
+    <br />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+      quibusdam, dolor, alias nostrum quos odit minus laudantium nisi, omnis
+      doloremque eligendi accusamus? Distinctio consectetur itaque sunt
+      dignissimos, minus illum at veniam sit maiores explicabo eligendi qui eum
+      repellendus magni magnam consequatur! Saepe eligendi doloremque dolorum
+      facilis odit expedita aspernatur fugit sapiente iste quidem blanditiis
+      ducimus, minima voluptatibus. Ea ut quibusdam voluptate temporibus
+      corrupti adipisci eligendi impedit, sint odit perspiciatis assumenda quod
+      velit ullam repellendus ex facere sunt quaerat architecto? Illum itaque
+      aperiam esse odit qui non, quia tenetur maiores maxime. Accusamus error
+      sit dolor perspiciatis nam, voluptas autem omnis illum!
+    </p>
+    <br />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+      quibusdam, dolor, alias nostrum quos odit minus laudantium nisi, omnis
+      doloremque eligendi accusamus? Distinctio consectetur itaque sunt
+      dignissimos, minus illum at veniam sit maiores explicabo eligendi qui eum
+      repellendus magni magnam consequatur! Saepe eligendi doloremque dolorum
+      facilis odit expedita aspernatur fugit sapiente iste quidem blanditiis
+      ducimus, minima voluptatibus. Ea ut quibusdam voluptate temporibus
+      corrupti adipisci eligendi impedit, sint odit perspiciatis assumenda quod
+      velit ullam repellendus ex facere sunt quaerat architecto? Illum itaque
+      aperiam esse odit qui non, quia tenetur maiores maxime. Accusamus error
+      sit dolor perspiciatis nam, voluptas autem omnis illum!
+    </p>
   </main>
 </template>
+<style scoped>
+h1.title {
+  text-transform: uppercase;
+}
+</style>
