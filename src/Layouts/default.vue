ce493ec63749b8303867e3c7d7fe3e9eabf95ec3
tomimandalap <tomi.mandala@privyid.tech>
2022-07-20 21:19:00 +0700

    fix: styling layout default

--- src/Layouts/default.vue
+++ src/Layouts/default.vue
@@ -1,3 +1,21 @@
 <template>
-  <RouterView />
+  <div class="layout-default">
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
 </template>
+<style scoped>
+div.layout-default {
+  min-height: 100vh;
+  max-width: 1904px;
+  margin: 0 auto;
+}
+div.content {
+  min-height: 100vh;
+  padding: 0 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
